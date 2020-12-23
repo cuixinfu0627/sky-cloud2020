4f70a3475a3fce29ff16e33702604570b9421a62
cuixinfu <870459550@qq.com>
2020-12-23 15:13:45 +0800

    新增springcloud-config 配置中心

---
+++ sky-cloud-config-client/src/main/java/com/sky/cloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.sky.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @name: ConfigClientController <tb>
+ * @title: 请输入类描述  <tb>
+ * @author: cuixinfu@51play.com <tb>
+ * @date: 2020/12/23 14:55 <tb>
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
