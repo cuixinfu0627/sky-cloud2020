cd2d8da910cbb52f7e064c5b064dbe2864cd7bf8
cuixinfu <870459550@qq.com>
2020-12-24 17:04:28 +0800

    新增spring-cloud-starter-stream-rabbit

---
+++ sky-cloud-stream-rabbitmq-provider/src/main/java/com/sky/cloud/service/IMessageProvider.java
@@ -0,0 +1,11 @@
+package com.sky.cloud.service;
+
+/**
+ * @name: IMessageProvider <tb>
+ * @title: 请输入类描述  <tb>
+ * @author: cuixinfu@51play.com <tb>
+ * @date: 2020/12/24 16:32 <tb>
+ */
+public interface IMessageProvider {
+    public String send();
+}
