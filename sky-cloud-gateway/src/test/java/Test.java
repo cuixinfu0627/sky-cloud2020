085d3d243ae24609bc1ff0d069fe3e250c3ea712
cuixinfu <870459550@qq.com>
2021-01-19 13:47:01 +0800

    新增测试文件

--- sky-cloud-gateway/src/test/java/Test.java
+++ sky-cloud-gateway/src/test/java/Test.java
@@ -10,8 +10,23 @@ public class Test {
 
     public static void main(String[] args) {
         ZonedDateTime now = ZonedDateTime.now();
-        System.out.println(now);
+        //System.out.println(now);
         //2020-12-22T15:27:57.999+08:00[Asia/Shanghai]
+
+        String str = "JSCA20120006";
+        char[] chars = "0123456789ABCDEF".toCharArray();
+        StringBuilder builder = new StringBuilder("");
+        byte[] bs = str.getBytes();
+        int bit;
+        for (int i = 0; i < bs.length; i++) {
+            bit = (bs[i] & 0x0f0) >> 4;
+            builder.append(chars[bit]);
+            bit = bs[i] & 0x0f;
+            builder.append(chars[bit]);
+            builder.append(' ');
+        }
+        System.out.println(builder.toString().trim());
+
     }
 
 }
